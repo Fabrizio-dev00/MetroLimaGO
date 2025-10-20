d429f72df637be066c9aa357ffd554b220f2ddb0
DeybyGonzales <deyby.gonzales@tecsup.edu.pe>
2025-10-19 22:40:26 -0500

    Implementación de Room: entidad, DAO y base de datos MetroLima

---
+++ app/src/main/java/com/miempresa/metrolimago/model/Estacion.kt
@@ -0,0 +1,12 @@
+package com.miempresa.metrolimago.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "estaciones")
+data class Estacion(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String,
+    val linea: String,
+    val distrito: String
+)
